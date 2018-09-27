935df0e07e21b32eb6e49f27e74b7b80bc6a7bb5
WMXPY <wm@mengw.io>
2018-09-27 17:26:15 -0500

    add similarity and tests

---
+++ src/util/string/common.ts
@@ -0,0 +1,18 @@
+/**
+ * @author WMXPY
+ * @namespace Util_String
+ * @fileoverview Common
+ */
+
+export const lastElement: <T>(arr: T[]) => T =
+    <T>(arr: T[]): T => arr[arr.length - 1];
+
+export const splitInput: (input: string) => string[] =
+    (input: string): string[] =>
+        (input.match(/\"\S[^\"]*\"|\S+/g) || [])
+            .map((str) => str.replace(/\"/g, ''))
+            .filter((s: string) => Boolean(s.trim()));
+
+export const spaces = (length: number) => {
+    return ' '.repeat(length);
+};
---
+++ src/util/string/similarity.ts
@@ -0,0 +1,29 @@
+/**
+ * @author WMXPY
+ * @namespace Util_String
+ * @fileoverview Similarity
+ */
+
+import { lastElement } from "#util/string/common";
+
+export const similar = (baseStr: string, targetStr: string): number => {
+    const base: string = baseStr.length < targetStr.length ? baseStr.toLowerCase() : targetStr.toLowerCase();
+    const target: string = baseStr.length < targetStr.length ? targetStr.toLowerCase() : baseStr.toLowerCase();
+
+    const next: (m: number[][], i: number, j: number) => number =
+        (m: number[][], i: number, j: number) => Math.min(m[i - 1][j - 1] + 1, m[i][j - 1] + 1, m[i - 1][j] + 1);
+
+    const map: number[][] =
+        new Array(base.length).fill(undefined)
+            .map(() => new Array(target.length).fill(undefined)
+                .map((value: undefined, index: number) => index));
+    for (let i: number = 1; i < base.length; i++) {
+        for (let j: number = 1; j < target.length; j++) {
+            map[i][j] = base.charAt(i - 1) === target.charAt(j - 1) ?
+                map[i - 1][j - 1] :
+                next(map, i, j);
+        }
+    }
+
+    return lastElement<number>(lastElement<number[]>(map || []) || []);
+};
